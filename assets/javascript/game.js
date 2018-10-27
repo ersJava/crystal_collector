6c2634157dfbac1b37d44001c99faa01aa6ba704
Elizabeth Sergeant <elizabethsergeant@Elizabeths-MacBook-Pro.local>
2018-10-26 22:43:54 -0400

    choco chip pancakes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,63 @@
 $(document).ready(function(){
 
-var win;
-var lose;
-var currentCount;
+// Global Variable
+var win = 0;
+var lose =0;
+var currentCount=0;
+var currentCrystalNum = Math.floor((Math.random() * 90) + 10);
+var currentCrystalCount;
 
-// ================================
-var currentCrystalNum;
+
+// Functions
 function currentNumber() {
     var a = document.getElementById("currentCrystalNum")
-    a.innerText = Math.floor((Math.random() * 30) + 10);
+    a.innerText = currentCrystalNum;
     }
-    currentNumber()
-//================================ 
-
-// *this is a loop that runs 4x and with console.log(crystals) will print crystals 4x
-for (var i = 1; i < 5; i++) {
-    var randomValue = Math.floor((Math.random() * 9) + 1);
-    $("#c" + i).attr('value' , randomValue)
-// declare a random var to generate a random num by math random
-
-
-var dataId = $(this).attr('value' , randomValue);
-console.log("dataId", dataId);
-        
-    }
-
-// doesn't this code work? ughhhhhhhhh
-$(startGame).ready(function() {
-// listens to a click event with anything with the class "crystals", once clicked it listens 
-$('.crystal').on('click', function() {
-
-// converting string of integers to a new number from the random value from each crystal
-// code doesn't work
-    // var num =parseInt($(this).attr(randomValue));
-
-    //current crystal count starts at 0, and adds number for var num
-    var currentCount =+ num
-    
-    //if currentCount === to currentCrystalNum
-    //alert "you win"
-    // win;++
-
-    //if else currentCount !== to currentCrystalNum
-    //alert "try again"
-    //lose++
-
-    // I don't know!!! 
-
-        });
-    });
-// ================================
-var currentCrystalCount;
+   
 function currentCount() {
     var a = document.getElementById("currentCrystalCount").html
-    // currentCount()
+
 }
-//================================ 
+
+for (var i = 1; i < 5; i++) {
+    var randomValue = Math.floor((Math.random() * 12) + 1);
+    $("#c" + i).attr('crystal-value' , randomValue)
+    console.log(randomValue);
+    }
 
 
-startGame();
+// Logic
+currentNumber();
+ 
+$('.crystal').on('click', function() {
 
+var crystalNumber = parseInt($(this).attr('crystal-value'));
+
+    currentCount += crystalNumber
+    $('#currentCrystalCount').text(currentCount)
+
+    if (currentCount === currentCrystalNum){
+        win++;
+        alert("Great Job, you win!")
+        $('#win').innerHTML("Win: " + win)
+    restartGame();
+
+    }else if (currentCount > currentCrystalNum){
+        lose++;
+        alert("Sorry, try again.")
+        $('#lose').innerHTML("Lose: " + lose);
+    restartGame();
+    }
+   // game is not restarting 
+   // the wins and loses are not being tracked :''( 
+
+
+    });
+
+function restartGame() {
+    currentNumber();
+    currentCount() 
+}
 
 });
 
